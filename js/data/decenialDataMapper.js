fa75a469b8aa7328c98f56b6ad7aab0b4a46741e
Luke Whyte <lukeawhyte@gmail.com>
2014-10-13 23:10:22 -0700

    Refined JSON parser

--- js/data/decenialDataMapper.js
+++ js/data/decenialDataMapper.js
@@ -1,6 +1,6 @@
 var input = data["census-api"].vars.var,
-	result = {
-		tunnels: {
+	tree = {
+		branches: {
 			topLevel: [
 				{
 					title: "Housing Variables",
@@ -17,46 +17,81 @@ var input = data["census-api"].vars.var,
 			]
 		}
 	},
-	tunnels = result.tunnels;
+	branches = tree.branches,
 
-input = filter(input, function (obj) { return obj["xml$id"].length > 7; });
+	filterInput = function (obj) {
+		var pre = obj["xml$id"].charAt(0);
+		return obj["xml$id"].length > 7 && (pre === 'H' || pre === 'P'); 
+	},
 
-each(input, function (obj, index, arr) { 
-	var id = obj["xml$id"],
-		result = {
-			title: obj.label,
-			desc: obj.concept,
-			gate: null,
-			key: null
-		},
-		createVar = function (parent) {
-			result.key = id;
-			tunnels[parent].push(result);
-		},
-		secLevel = (id.charAt(1) === "C") ? id.slice(0, 6) : id.slice(0, 4);
-		thirdLevel = secLevel + id.charAt(secLevel.length);
+	addToTree = function (obj, index, arr) {
+		var id = obj["xml$id"],
+			output = {
+				label: obj.label,
+				concept: obj.concept,
+				gate: null,
+				key: null
+			},
+			secLevel = (id.charAt(1) === "C") ? id.slice(0, 6) : id.slice(0, 4),
+			thirdLevel = secLevel + id.charAt(secLevel.length),
 
-	if (!tunnels[secLevel]) tunnels[secLevel] = [];
+			createVar = function (parent) {
+				output.key = id;
+				branches[parent].push(output);
+			},
 
-	if (/^[a-z]+$/i.test(id.charAt(secLevel.length))) {
-	 if (!tunnels[thirdLevel]) {
-			var toAdd = {};
-			each(arr, function (obj) {
-				if (obj["xml$id"] === thirdLevel + "0001" || obj["xml$id"] === thirdLevel + "001") {
-					toAdd = {
-						title: obj["label"],
-						desc: obj["concept"],
-						gate: thirdLevel
-					};
-					tunnels[secLevel].push(toAdd);
-				}
-			});
-			tunnels[thirdLevel] = [];
+			createGate = function () {
+				var toAdd = {};
+				each(arr, function (obj) {
+					if (obj["xml$id"] === thirdLevel + "0001" || obj["xml$id"] === thirdLevel + "001") {
+						newGate = {
+							label: obj["label"],
+							concept: obj["concept"],
+							gate: thirdLevel
+						};
+						branches[secLevel].push(newGate);
+					}
+				});
+				branches[thirdLevel] = [];
+			};
+
+		if (!branches[secLevel]) branches[secLevel] = [];
+
+		if (/^[a-z]+$/i.test(id.charAt(secLevel.length))) {
+		 if (!branches[thirdLevel]) {
+				createGate();	
+			}
+			createVar(thirdLevel);
+		} else {
+			createVar(secLevel);
 		}
-		createVar(thirdLevel);
-	} else {
-		createVar(secLevel);
-	}
+	},
+
+	formatBranchStrings = function (obj, key, arr) {
+		var parse = {
+			label: function (str) {
+				str = str.replace(/:/g, '');
+				str = str.replace(/!!/g, '>');
+				return str;
+			},
+			concept: function (str) {
+				str = str.replace(/^(.*?)\. /, '');
+				obj.cells = str.slice(str.indexOf('[') + 1, str.indexOf(']'));
+				str = str.slice(0, str.indexOf('[')).trim();
+				return str.charAt(0) + str.slice(1).toLowerCase();
+			}
+		};
+		if (typeof obj.label !== 'undefined') obj.label = parse.label(obj.label);
+		if (typeof obj.concept !== 'undefined') obj.concept = parse.concept(obj.concept);
+	};
+
+_.chain(input)
+	.filter(filterInput)
+	.each(addToTree)
+	.value();
+
+map(tree.branches, function (arr) {
+	if (isArray(arr)) each(arr, formatBranchStrings);
 });
 
-result
+tree
